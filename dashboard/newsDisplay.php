533f2a23930a9a29abb0555dba5854e91da8051c
swizzmeister <lcheyne@rogers.com>
2023-04-18 01:09:35 -0300

    started the newsDisplay.php gotta find a way to call the function from another page

--- dashboard/newsDisplay.php
+++ dashboard/newsDisplay.php
@@ -5,6 +5,20 @@ function my_autoloader($class)
 }
 
 spl_autoload_register('my_autoloader');
- echo '<div class="newsDisplay">
-            
+function displayNewsArticle($artNum)
+{
+    $myFile = "news.byt";
+    $data = "";
+    file_get_contents($myFile,$data);
+    $articals = unserialize($data);
+    if ($artNum<count($articals)){
+        echo '<div class="newsDisplay" xmlns="http://www.w3.org/1999/html">
+            <img src="' .$articals[$artNum]->image.'" alt="News Artical Image" class="newsImage">
+            <h2>'.$articals[$artNum]->headline.'</h2>
+            <p>'.$articals[$artNum]->author.'</p></br>
+            <p>'.$articals[$artNum]->text.'</p>
         </div> ';
+    }
+
+}
+
